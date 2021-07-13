27d44de31ff330f8698cae0c2a6c91fc9c64db5f
Jordan Thomas <tjordan706@gmail.com>
2021-07-13 00:20:45 -0400

    commit tree f356d8b49b2bd692d16585035e048c98d8edd40b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+fetch(imgUrl, { method: 'GET' }).then((response) => {
+  console.log(response);
+  if (response.ok) {
+    return response.json();
+  }
+});
